2b97cda1d4863c821ff02ecce779e157070f0833
Mariam(Manill) Mohamed <minniekim752@gmail.com>
2023-10-24 16:42:29 -0700

    Task has been completed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s), sizeof(char)\n");
+	printf("Size of an int: %d byte(s), sizeof(int)\n");
+	printf("Size of a lone int: %d byte(s), sizeof(long int)\n");
+	printf("Size of a long long int: %d byte(s), sizeof(long long int)\n");
+	printf("Size of a float: %d byte(s), sizeof(float)\n");
+	return (0);
+}
