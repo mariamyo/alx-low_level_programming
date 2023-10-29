449e25fc1af88ff89fc5ed8a7474327f1670aab1
Mariam(Manill)Mohamed <minniekim752@gmail.com>
2023-10-29 08:40:18 -0700

    Task is done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+		putchar(digit + '0');
+	putchar('\n');
+	return (0);
+}
