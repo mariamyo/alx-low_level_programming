41774c5dc206ab8b323293476ee95e20d3d00370
Mariam(Manill)Mohamed <minniekim752@gmail.com>
2023-10-30 15:37:56 -0700

    Task is done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,13 @@ int main(void)
 
 	for (digit = 0 ; digit <= 9 ; digit++)
 	{
-		if (digit == 9)
-			putchar(digit + '0');
-		else
+		putchar(digit + '0');
+		if (digit != 9)
 		{
-			putchar(digit + '0');
 			putchar(',');
 			putchar(' ');
 		}
+		putchar('\n');
 	}
 	return (0);
 }
