4bd05026cbad4363fa2f8f2e35ae9995c54aab18
Mariam(Manill)Mohamed <minniekim752@gmail.com>
2023-11-01 11:53:35 -0700

    Task has been completed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,21 @@
 
 /**
  * print_alphabet_x10 - print alpha 10times
- * 
+ *
  * Return: -
 */
 void print_alphabet_x10(void)
 {
-    int alpha_x10;
-    int i;
+	int alpha_x10;
+	int loop;
 
-    i = 0;
+	loop = 0;
 
-    while (i < 10)
-    {
-        for (alpha_x10 = 'a' ; alpha_x10 <= 'z' ; alpha_x10++)
-        _putchar(alpha_x10);
-    }
-    _putchar('\n');
+	while (loop < 10)
+	{
+		loop++;
+		for (alpha_x10 = 'a' ; alpha_x10 <= 'z' ; alpha_x10++)
+			_putchar(alpha_x10);
+		_putchar('\n');
+	}
 }
